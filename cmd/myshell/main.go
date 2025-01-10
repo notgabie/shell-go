2a722e4b289c7664d932b606ee83d4df483363ad
notgabie <gabriellamondvay@gmail.com>
2025-01-11 00:08:59 +0100

    add handling for invalid commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -15,4 +15,10 @@ func main() {
 
 	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+	fmt.Println(command[:len(command) - 1] + ": command not found")
 }

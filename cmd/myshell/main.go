091b917d38e17d749171e810ed495d19e5fa1223
notgabie <gabriellamondvay@gmail.com>
2025-01-11 13:33:48 +0100

    add loop so shell runs until exited

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,15 +10,13 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	 fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+	for {
+		fmt.Print("myshell> ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		fmt.Println(command[:len(command) - 1] + ": command not found")
 	}
-	fmt.Println(command[:len(command) - 1] + ": command not found")
 }

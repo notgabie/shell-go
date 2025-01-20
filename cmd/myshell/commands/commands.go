bea2db50213697e0f6081f413b3ea73de91635ef
notgabie <gabriellamondvay@gmail.com>
2025-01-20 12:56:44 +0100

    codecrafters submit [skip ci]

--- cmd/myshell/commands/commands.go
+++ cmd/myshell/commands/commands.go
@@ -10,4 +10,5 @@ var CommandRegistry = map[string]CommandInfo{
 	"exit": {Name: "exit", Type: "shell builtin", Description: "Exit the shell"},
 	"echo": {Name: "echo", Type: "shell builtin", Description: "Print the arguments to the standard output"},
 	"type": {Name: "type", Type: "shell builtin", Description: "Display information about command type"},
+	"pwd": {Name: "pwd", Type: "shell builtin", Description: "Print the current working directory"},
 }
